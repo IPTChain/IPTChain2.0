4de1bcfbd2835c4607e1713a26694b37cf5e70e3
chrisFang55 <1198331043@qq.com>
2018-07-04 10:53:20 +0800

    change resources

--- consensus/ebft/consensusMessageType.go
+++
@@ -1,9 +0,0 @@
-package ebft
-
-type ConsensusMessageType byte
-
-const (
-	ChangeViewMsg ConsensusMessageType = 0x00
-	PrepareRequestMsg ConsensusMessageType = 0x20
-	PrepareResponseMsg ConsensusMessageType = 0x21
-)
--- contracts/errors/errors.go
+++
@@ -1,9 +0,0 @@
-package errors
-
-import "errors"
-
-var (
-	ErrAssetNameInvalid      = errors.New("asset name invalid")
-	ErrAssetAmountInvalid    = errors.New("asset amount invalid")
-	ErrAssetPrecisionInvalid = errors.New("asset precision invalid")
-)
